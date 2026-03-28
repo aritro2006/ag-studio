fca7c7e9cc25b9028e76dff1df04224ba207abc0
aritro2006 <aritroghosh2906@gmail.com>
2026-03-28 23:36:01 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+const contactRoute = require('./routes/contact');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', contactRoute);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
